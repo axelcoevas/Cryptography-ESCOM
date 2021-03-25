89dc60e79ef0eae10c334c155871d197171bb71f
Axel Cuevas <axelcoevas@gmail.com>
2021-03-24 19:43:24 -0600

    Terminando p1

--- Prácticas/practica1/index.js
+++ Prácticas/practica1/index.js
@@ -1,12 +1,31 @@
 let vigenereText;
+let affineText;
 
-function affine() {
+function vigenere() {
   let text = vigenereText;
   let key = getVigenereKey();
   let filename = getVigenereFileName();
   let cipher = getVigCipher();
 
   let m = vigenereCipher(text, key, cipher);
+
+  download(filename, m);
+}
+
+function affine() {
+  let n = document.getElementById('affN').value;
+  let alpha = document.getElementById('affA').value;
+  let beta = document.getElementById('affB').value % n;
+  let cipher = getAffCipher();
+  let filename = getAffineFileName();
+  let text = affineText;
+
+  if (!validAffineKey(n, alpha)) {
+    alert('Por favor elije un valor valido para Alpha');
+    return;
+  }
+
+  let m = affineCipher(text, alpha, beta, n, cipher);
 
   download(filename, m);
 }
@@ -39,15 +58,8 @@ function getVigenereText(file) {
   vigenereText = file;
 }
 
-function getVigCipher() {
-  let radio = document.getElementsByName('vigRadio');
-  let value;
-  for (let i = 0; i < radio.length; i++) {
-    if (radio[i].checked)
-      value = radio[i].value === 'true';
-  }
-
-  return value;
+function getAffineText(file) {
+  affineText = file;
 }
 
 function getVigenereFileName() {
@@ -63,24 +75,47 @@ function getVigenereFileName() {
   return vigenereFilename;
 }
 
+function getAffineFileName() {
+  let affineFilename = document.getElementById('affFile').files[0].name;
+
+  let result = affineFilename.split(".");
+  if (getAffCipher()) {
+    affineFilename = result[0] + ".aff";
+  } else {
+    affineFilename = result[0] + ".txt";
+  }
+
+  return affineFilename;
+}
+
 function getVigenereKey() {
   let key = document.getElementById('vigKey').value;
   return key;
 }
 
-function getNeutroKey(k) {
-  let neutro = "";
-  k = k.toUpperCase();
-  for (let i = 0; i < k.length; i++) {
-    let c = k.charCodeAt(i) - 65;
-    for (let j = 0; j < 26; j++) {
-      if ((c + j) % 26 == 0) {
-        neutro += String.fromCharCode(j + 65);
-      }
-    }
+function getVigCipher() {
+  let radio = document.getElementsByName('vigRadio');
+  let value;
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked)
+      value = radio[i].value === 'true';
   }
-  return neutro;
+
+  return value;
 }
+
+function getAffCipher() {
+  let radio = document.getElementsByName('affRadio');
+  let value;
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked)
+      value = radio[i].value === 'true';
+  }
+
+  return value;
+}
+
+//Vigenere functions
 
 function vigenereCipher(text, key, cipher) {
   let m = '';
@@ -103,7 +138,23 @@ function vigenereCipher(text, key, cipher) {
   return cipher ? m.toUpperCase() : m.toLowerCase();
 }
 
-function validAffineKey(a, b) {
+function getNeutroKey(k) {
+  let neutro = "";
+  k = k.toUpperCase();
+  for (let i = 0; i < k.length; i++) {
+    let c = k.charCodeAt(i) - 65;
+    for (let j = 0; j < 26; j++) {
+      if ((c + j) % 26 == 0) {
+        neutro += String.fromCharCode(j + 65);
+      }
+    }
+  }
+  return neutro;
+}
+
+//Affine functions
+
+function validAffineKey(a, b) { //  Euclides's algorithm
   let x = a;
   let y = b;
 
@@ -116,7 +167,7 @@ function validAffineKey(a, b) {
   return x === 1;
 }
 
-function multiplicativeInverse(a, n) {
+function multiplicativeInverse(a, n) {  // Euclides's extended Algorithm
   let r = [a, n];
   let s = [1, 0];
   let t = [0, 1];
@@ -135,28 +186,24 @@ function multiplicativeInverse(a, n) {
 }
 
 function affineCipher(text, key, b, n, cipher) {
-  text = text.toUpperCase();
-  if (!validAffineKey(key, n)) {
-    return 'Llave inválida'
-  }
-
   let m = '';
 
   if (!cipher) {
     key = multiplicativeInverse(key, n);
     b = n - b;
     for (let i = 0; i < text.length; i++) {
-      let p = text.charCodeAt(i);
+      let p = text.charCodeAt(i) - 33;
       let c = String.fromCharCode((key * (p + b)) % n);
       m += c;
     }
   } else {
     for (let i = 0; i < text.length; i++) {
-      let p = text.charCodeAt(i) - 65;
-      let c = String.fromCharCode((key * p + b) % n);
+      let p = text.charCodeAt(i);
+      let c = String.fromCharCode((key * p + b) % n + 33);
       m += c;
     }
   }
 
-  return cipher ? m.toUpperCase() : m.toLowerCase();
+  return m;
 }
+//  Validations for inputs
